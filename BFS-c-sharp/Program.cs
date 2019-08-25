12fd304ca3db129ddf5f008a067eab6ee69c2801
garyAdam <43093216+garyAdam@users.noreply.github.com>
2019-08-25 18:12:50 +0200

    getdistance works

--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -13,11 +13,38 @@ namespace BFS_c_sharp
 
             foreach (var user in users)
             {
-                Console.WriteLine(user);
+                Console.WriteLine(user + "friends : ");
+                foreach (var item in user.Friends)
+                {
+                    Console.WriteLine(item);
+                }
             }
-
+            Console.WriteLine(GetDistance(users[0],users[2]));
             Console.WriteLine("Done");
             Console.ReadKey();
+        }
+
+        static int GetDistance(UserNode userNode1, UserNode userNode2)
+        {
+            Queue<UserNode> userNodes = new Queue<UserNode>();
+            userNodes.Enqueue(userNode1);
+            int counter = 0;
+            while (userNodes.Count>0)
+            {
+                UserNode userNode = userNodes.Dequeue();
+                if (userNode==userNode2)
+                {
+                    return counter;
+                }
+                foreach (var item in userNode.Friends)
+                {
+                    userNodes.Enqueue(item);
+                }
+                counter++;
+            }
+
+
+            return -1;
         }
     }
 }
